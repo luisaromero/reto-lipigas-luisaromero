8b93026dca8bdecb9c415dc40f301ab21620ac1b
luisaromero <luisa.marcela.romero@gmail.com>
2020-07-17 23:36:18 -0400

    agregando vistas home , registro y validacion

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Radio } from 'antd';
+
+export default function Home() {
+    return (
+      <div className="">
+     <Button type="primary" >
+          Primary
+        </Button>
+        <Button type="primary" >
+          Primary
+        </Button>
+      </div>
+    );
+  }
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Register from './Components/Register'
+import Register from './Components/Register';
+import Validation from './Components/Validation'
+import Home from './Components/Home'
+
 
 
 const Routes = () =>{
     return(
         <Router>
         <Switch>
-            <Route exact path='/' component = {Register}/>
+            <Route exact path='/' component = {Home}/>
             <Route exact path='/registro' component = {Register}/>  
+            <Route exact path='/validar' component = {Validation}/>  
         </Switch>
         </Router>
     )
